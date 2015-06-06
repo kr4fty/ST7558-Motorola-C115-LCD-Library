34114f421bc016dfd1ebe81acb0b6123b113a627
kr4fty <technicc@gmail.com>
2015-06-06 17:16:05 -0300

    ST7558 I2C display

--- ST7558.h
+++ ST7558.h
@@ -1,3 +1,33 @@
+/***************************************************
+  This is a library for the ST7558 i2c display.
+
+  These displays use I"Cto communicate, 2 or 3 pins are required to
+  interface (RST is optional)
+  
+  Based on the driver written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+
+  It depends on libraries Adafruit-GFX
+
+  Author: Tapia Favio: technicc(at)gmail.com
+ 
+ Pinout:
+ 
+	Lcd chip: ST7558 - Wintek Lcd
+	Resolution: 94x64
+	Technology: FSTN
+	I2C write address: 0x78 - (0c3C with Wire arduino funtion)
+
+    Pin1 Vdd +2.8V		+-----------------------------+
+    Pin2 Res (n/c)		|                             |
+    Pin3 Sclk			    |        Motorola C115        |
+    Pin4 Sda 			    |            102x65           |
+    Pin5 A0 (Gnd)		  |       (visible 94x64)       |
+    Pin6 Gnd			    +-----------------------------+
+    Pin7 Vlcd +12V		     |  |  |  |  |  |  |  |
+    Pin8 Reset			       1  2  3  4  5  6  7  8 
+
+ ****************************************************/
 #ifndef _ST7558_H
 #define _ST7558_H
 
